88a5d176cbc0720d88431e312638a491c25fa9b5
Tejesh N <Tejesh.N@odessainc.com>
2026-03-03 15:35:13 +0530

    Added delegate test

---
+++ Programs/DelegateTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs
+{
+    internal class DelegateTest
+    {
+        //public delegate int SquareOfNumber(int x);
+
+        public static int square(int x)
+        {
+            return x * x;
+        }
+        public static void Main()
+        {
+            //SquareOfNumber squareOf = new SquareOfNumber(square);
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine($"Square of number {i}:{squareOf.Invoke(i)}");
+            //}
+
+            //SquareOfNumber squareOf = square;
+
+            //SquareOfNumber squareOf= delegate(int x)
+            //{
+            //    return x*x; 
+            //};
+
+            //SquareOfNumber squareOf = x => x * x;
+
+            Func<int, int> squareOf = x => x * x;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Square of number {i}:{squareOf.Invoke(i)}");
+            }
+        }
+
+    }
+}
